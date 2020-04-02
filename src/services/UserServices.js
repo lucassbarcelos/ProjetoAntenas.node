8ed1d107ec508aac18dfb69d14ef162b27ec3e7f
Lucas Barcelos <lucasbarcelos@MacBook-Pro-de-Lucas.local>
2020-04-01 22:22:12 -0300

    authentication com jwt

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,5 +1,6 @@
 const connection = require("../database/connection");
 const generateId = require("../utils/generateId");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   async create(request, response) {
@@ -35,5 +36,25 @@ module.exports = {
   async index(request, response) {
     const users = await connection("user").select("*");
     return response.json({ users });
+  },
+
+  async logon(request, response) {
+    const { email, password } = request.body;
+
+    const token = jwt.sign(password, "teste");
+    return response.json({ token });
+  },
+
+  async teste(request, response) {
+    const { token } = request.headers;
+
+    const res = jwt.decode(token, "teste");
+    if (!res) {
+      return response.status(401).json({
+        error: "Falha na autenticação no token"
+      });
+    }
+
+    return response.json({ res });
   }
 };
