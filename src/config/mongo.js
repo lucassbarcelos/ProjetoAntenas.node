cdbc1133487dbf2f68da7c6e6866b0bd1ed63489
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-09-20 15:19:52 -0300

    fix: script start

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -5,7 +5,7 @@ const parsedUser = encodeURIComponent(MONGODB_USER);
 const parsedPassword = encodeURIComponent(MONGODB_PASS);
 
 module.exports = {
-  uri: `mongodb://${parsedUser}:${parsedPassword}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}?authSource=admin`,
+  uri: `mongodb+srv://antenas:fatec@clusterantenas.sd3th.gcp.mongodb.net/antenas?retryWrites=true&w=majority`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
