2336bdfc544565fbfede20d410bbe002cf545ec6
Matheus Rocha <matheus_rocha98@outlook.com>
2020-11-03 21:59:59 -0300

    build: remove bodyParser dependency

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const helmet = require('helmet')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.json())
 app.use(helmet())
